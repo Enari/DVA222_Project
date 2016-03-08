c2b1184c41859665ffa78ce6a1d33ac94e0431b1
Enari <Enari@minit.nu>
2016-03-08 12:06:51 +0100

    Desing of the Container class

--- DVA222_Project/DVA222_Project/Container.h
+++ DVA222_Project/DVA222_Project/Container.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "ControlBase.h"
+#include <vector>
+
 class Container :
   public ControlBase
 {
 public:
-  Container();
+  Container(int y, int x, int w, int h);
+  Container(int y, int x, int w, int h, int z);
   ~Container();
 private:
+  vector<ControlBase>* objects;
   int Z;
 };
 
