c27aeabb3dcadcb22c35e4d7bdeb8f9952f06e13
deerel <deerel@gmail.com>
2016-03-09 13:04:01 +0100

    Added extra constructors for RadioButton and Checkbox class.

--- DVA222_Project/DVA222_Project/Checkbox.h
+++ DVA222_Project/DVA222_Project/Checkbox.h
@@ -1,10 +1,17 @@
 #pragma once
 #include "Selector.h"
+#include <string>
+
 class Checkbox :
   public Selector
 {
 public:
-  Checkbox(Label label);
+  Checkbox();
+  Checkbox(string text);
+  Checkbox(string text, Color& color);
+  Checkbox(string text, int x, int y);
+  Checkbox(string text, Color& color, int x, int y);
+  Checkbox(const Checkbox& other);
   ~Checkbox();
 private:
 };
