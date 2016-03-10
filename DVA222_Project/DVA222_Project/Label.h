74af7aa78a091d2f9fbc6308f266125d15324dcc
Enari <Spam@minit.nu>
2016-03-10 16:04:05 +0100

    Label is Done

--- DVA222_Project/DVA222_Project/Label.h
+++ DVA222_Project/DVA222_Project/Label.h
@@ -3,25 +3,31 @@
 #include "ZControlBase.h"
 #include <string>
 #include "Color.h"
+#include "Graphix.h"
 
 class Label:
   public ZControlBase
 {
 public:
 	Label();
-	Label(const Label& other);
   Label(string text);
   Label(string text, int x, int y);
   Label(string text, int x, int y, int red, int green, int blue);
   Label(string text, Color& color, int x, int y);
+  Label(const Label& other);
   ~Label();
+  
+  //Set&get's
   void SetText(string text);
 	string GetText();
   Color GetTextColor();
-  void SetTextColor(Color color);
+  void SetTextColor(Color& color);
   void SetTextColor(int red, int green, int blue);
+  void SetPosition(int x, int y);
+
+  //Event Overlaods
 	void OnPaint();
-	void SetPosition(int x, int y);
+	
 
 private:
 	string text;
