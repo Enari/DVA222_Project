4ba025f390cfa119a4614fda453c3ebf5bde98e4
pwn15002 <pontus.wed@gmail.com>
2016-03-11 11:59:18 +0100

    Layout i dragable window fixad

--- DVA222_Project/DVA222_Project/DVA222_Project.cpp
+++ DVA222_Project/DVA222_Project/DVA222_Project.cpp
@@ -31,32 +31,32 @@ int _tmain(int argc, char** argv)
   //------------------------------------------------------------------------------------------------------------------------------------
 
   //Skapa MainWindow
-  Window* mainWindow = new Window("I am a dragable window", Color(255, 252, 255), Color(255, 0, 255), Color(0, 252, 255), 100, 300, 600, 300);
+  Window* mainWindow = new Window("I am a dragable window", Color(255, 252, 255), Color(255, 0, 255), Color(0, 252, 255), 50, 50, 700, 500);
 
   //Create Window
   //Window* win = new Window("I am a dragable window", Color(255,252,255), Color(255, 0, 255), Color(0, 252, 255), 100, 300, 600, 300);
   //win->AddObject(new Label("I am a Label in a window..", 50, 30, 255, 32, 32));
   
   //Create Content
-  ZControlBase* topText = new Label("I am a Label. Actually all the text that you see down there are Labels contained in the Panels. My text has a different color", 30, 30, 0, 0, 255);
-  ZControlBase* image = new ImageBox("DaloGoatBig.bmp", 30, 100);
-  ZControlBase* freeRadioButton = new RadioButton("I am a free RadioButton. This Window is my container", 50, 150);
-  ZControlBase* freeCheckbox = new Checkbox("I am a free CheckBox.", 50, 130);
+  ZControlBase* topText = new Label("I am a Label. Actually all the text that you see down there are Labels contained in the Panels. My text has a different color", 5, 45, 0, 0, 255);
+  ZControlBase* image = new ImageBox("DaloGoatBig.bmp", 30, 350, 112, 120);
+  ZControlBase* freeRadioButton = new RadioButton("I am a free RadioButton. This Window is my container.", 30, 190);
+  ZControlBase* freeCheckbox = new Checkbox("I am a free CheckBox.", 30, 210);
   ZControlBase* freeButton = new Button("I am a button imprissoned in the panel", Color(0, 0, 0), Color(255, 255, 100), 30, 80, 320, 30);
 
   //Create pannels
-  Panel* greenPanel = new Panel(400, 300, 370, 200, Color(0, 255, 110));
-  greenPanel->AddObject(new Label("I am also a Panel. as you can see...", 30, 30, 0, 0, 0));
+  Panel* greenPanel = new Panel(250, 300, 405, 120, Color(0, 255, 110));
+  greenPanel->AddObject(new Label("I am also a Panel. As you can see i'm behind the other panel.", 30, 30, 0, 0, 0));
   greenPanel->AddObject(new Checkbox("I am a Captured CheckBox.", 30, 50));
 
   //Panel* bluePanel = new Panel(250, 450, 405, 120, Color(0, 200, 255));
-  Panel* bluePanel = new Panel(30, 10, 405, 120, Color(0, 200, 255));
+  Panel* bluePanel = new Panel(200, 350, 405, 120, Color(0, 200, 255));
   bluePanel->AddObject(new Label("I am a Panel. I can contain stuff inside (like the button below).", 30, 30, 0, 0, 0));
   bluePanel->AddObject(new Label("I can also have different colors.", 30, 48, 0, 0, 0));
   bluePanel->AddObject(new Button("I am a button imprissoned in the panel", Color(0, 0, 0), Color(255, 255, 100), 30, 80, 320, 30));
 
   //Test checkboxGroup
-  CheckboxGroup* checkboxGroup = new CheckboxGroup("I am a Checkbox Group", 30, 70, 365);
+  CheckboxGroup* checkboxGroup = new CheckboxGroup("I am a Checkbox Group", 30, 70, 250);
   checkboxGroup->AddObject(new Checkbox("I am a CheckBox."));
   checkboxGroup->AddObject(new Checkbox("I am another CheckBox."));
   Checkbox* selectedCheckbox1 = new Checkbox("I am a selected CheckBox.");
@@ -67,7 +67,7 @@ int _tmain(int argc, char** argv)
   checkboxGroup->AddObject(selectedCheckbox2);
 
   //Test RadioButtonGroup
-  RadioButtonGroup* radioButtonGroup = new RadioButtonGroup("I am a RadioButton Group", 405, 70, 365);
+  RadioButtonGroup* radioButtonGroup = new RadioButtonGroup("I am a RadioButton Group", 300, 70, 365);
   radioButtonGroup->AddObject(new RadioButton("I am a RadioButton."));
   radioButtonGroup->AddObject(new RadioButton("I am another RadioButton."));
   RadioButton* selectedadioButton = new RadioButton("I am The selected RadioButton. The One and only.");
@@ -76,14 +76,14 @@ int _tmain(int argc, char** argv)
   radioButtonGroup->AddObject(new RadioButton("I am just another poor RadioButton."));
   
   //Add objects to main window
-  //mainWindow->AddObject(topText);
-  //mainWindow->AddObject(image);
-  //mainWindow->AddObject(freeRadioButton);
-  //mainWindow->AddObject(freeCheckbox);
+  mainWindow->AddObject(topText);
+  mainWindow->AddObject(image);
+  mainWindow->AddObject(freeRadioButton);
+  mainWindow->AddObject(freeCheckbox);
   //mainWindow->AddObject(freeButton);
-  //mainWindow->AddObject(static_cast<ZControlBase*>(greenPanel));
+  mainWindow->AddObject(static_cast<ZControlBase*>(greenPanel));
   mainWindow->AddObject(static_cast<ZControlBase*>(bluePanel));
-  //mainWindow->AddObject(static_cast<ZControlBase*>(checkboxGroup));
+  mainWindow->AddObject(static_cast<ZControlBase*>(checkboxGroup));
   mainWindow->AddObject(static_cast<ZControlBase*>(radioButtonGroup));
   //mainWindow->AddObject(win);
 
