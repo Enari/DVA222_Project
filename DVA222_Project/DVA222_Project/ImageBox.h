ce97105d474cb55e21e30995a6e5f9e404e73569
pwn15002 <pontus.wed@gmail.com>
2016-03-11 14:30:53 +0100

    Tog bort tom constructor i ImageBox

--- DVA222_Project/DVA222_Project/ImageBox.h
+++ DVA222_Project/DVA222_Project/ImageBox.h
@@ -8,7 +8,6 @@ private:
 	string filepath;
 	Bitmap *image;
 public:
-  ImageBox();
   ImageBox(const ImageBox &other);
   ImageBox(string filePath, int posX, int posY);
   ImageBox(string filePath, int posX, int posY, int width, int height);
